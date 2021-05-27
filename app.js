e5f53b3085c09f028bc817b699013f2f971e1a49
Ruchir Rathore <rathoreruchir2@gmail.com>
2021-05-28 04:14:14 +0530

    updated app.js

--- app.js
+++ app.js
@@ -43,6 +43,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 if(process.env.NODE_ENV == "production"){
   app.use(express.static("client/build"))
+
+  app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); //relative path
+})
 }
 
 //Passport initialization for authentication
