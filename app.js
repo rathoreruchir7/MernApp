bc9cf06d364f1c8cffca3b8a33f709cbb995d162
Ruchir Rathore <rathoreruchir2@gmail.com>
2021-05-28 12:22:23 +0530

    removed the code added before

--- app.js
+++ app.js
@@ -44,9 +44,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 if(process.env.NODE_ENV == "production"){
   app.use(express.static("client/build"))
 
-  app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); //relative path
-})
+//   app.get('*', (req, res)=>{
+//     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); //relative path
+// })
 }
 
 //Passport initialization for authentication
