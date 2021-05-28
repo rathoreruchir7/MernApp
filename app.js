f98a14fe3c0a99eabe4583b89fb14f8d1d701abf
Ruchir Rathore <rathoreruchir2@gmail.com>
2021-05-28 13:25:43 +0530

    testing

--- app.js
+++ app.js
@@ -44,8 +44,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 if(process.env.NODE_ENV == "production"){
   app.use(express.static(path.join(__dirname, "client/build")))
 
-  app.get('*', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')); //relative path
+  app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); //relative path
 })
 }
 
