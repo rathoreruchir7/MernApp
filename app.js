8bae8a6fa2290b98b4f01e205ea09cf25b589daa
Ruchir Rathore <rathoreruchir2@gmail.com>
2021-05-28 13:04:22 +0530

    added app.get

--- app.js
+++ app.js
@@ -39,14 +39,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 if(process.env.NODE_ENV == "production"){
   app.use(express.static(path.join(__dirname, "client/build")))
 
-//   app.get('*', (req, res)=>{
-//     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); //relative path
-// })
+  app.get('*', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')); //relative path
+})
 }
 
 //Passport initialization for authentication
